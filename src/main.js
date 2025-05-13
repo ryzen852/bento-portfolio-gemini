f58e8d772a652da70b19c5f997a7b480b43cf7c9
ryzen852 <ryzen852@gmail.com>
2025-05-13 14:31:33 -0700

    use devicon package instead link rel stylesheet

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import './style.css' 
-import 'bootstrap/dist/css/bootstrap.min.css' 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import * as devicon from "devicon";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
